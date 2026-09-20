32a58a73f98eb0e040abda3ceb23be262b66c468
nakayama2536-happy <nakayama2536@gmail.com>
2026-09-20 09:11:50 +0900

    Update service worker cache to v4.8

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "india1400-v4-7-20260920";
-const SHELL = ["./?v=4.7", "index.html", "manifest.webmanifest", "icon-192.png", "icon-512.png"];
+const CACHE = "india1400-v4-8-20260920";
+const SHELL = ["./?v=4.8", "index.html", "manifest.webmanifest", "icon-192.png", "icon-512.png"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting()));
