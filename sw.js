659209016cb9a3d76d6c48d1fb53410ea05e1106
モゲパーマン <nakayama2536@gmail.com>
2026-09-22 15:49:35 +0900

    Refresh PWA cache for v5.5

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "india1400-v5-4-20260922";
-const SHELL = ["./?v=5.4", "index.html", "manifest.webmanifest", "apple-touch-icon.png", "icon-192.png", "icon-512.png"];
+const CACHE = "india1400-v5-5-20260922";
+const SHELL = ["./?v=5.5", "index.html", "manifest.webmanifest", "apple-touch-icon.png", "icon-192.png", "icon-512.png"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting()));
